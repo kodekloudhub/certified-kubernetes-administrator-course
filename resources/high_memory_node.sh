6b5d7e9ce8bee04c24c8236f738f215915dbc425
vpalazhi <32654249+vpalazhi@users.noreply.github.com>
2022-12-19 16:07:10 -0500

    Create high_memory_node.sh

---
+++ resources/high_memory_node.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo cluster1 $(kubectl --context cluster1 top node --no-headers | sort -nr -k3 | head -1) > /tmp/high_memory_node
+echo cluster2 $(kubectl --context cluster2 top node --no-headers | sort -nr -k3 | head -1) >> /tmp/high_memory_node
+echo cluster3 $(kubectl --context cluster3 top node --no-headers | sort -nr -k3 | head -1) >> /tmp/high_memory_node
+echo cluster4 $(kubectl --context cluster4 top node --no-headers | sort -nr -k3 | head -1) >> /tmp/high_memory_node
+final_value=$(cat /tmp/high_memory_node | sort -nr -k 4 | awk '{print $1,$2}' | head -1 | tr " " ,)
+if [[ $(cat /opt/high_memory_node | grep $final_value) ]]
+then 
+        echo SUCCESS
+else
+        echo FAIL
+fi
