024b9ef25b7e383f71fdf542bea911e6819a93f5
vpalazhi <32654249+vpalazhi@users.noreply.github.com>
2022-12-29 09:51:54 -0600

    Update high_memory_pod.sh

--- resources/high_memory_pod.sh
+++ resources/high_memory_pod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo cluster1 $(kubectl --kubeconfig /root/.kube/clusters/cluster1_config top pods -A --no-headers | sort -nr -k4 | head -1) > /tmp/high_memory_pod
-echo cluster2 $(kubectl --kubeconfig /root/.kube/clusters/cluster2_config top pods -A --no-headers | sort -nr -k4 | head -1) >> /tmp/high_memory_pod
-echo cluster3 $(kubectl --kubeconfig /root/.kube/clusters/cluster3_config top pods -A --no-headers | sort -nr -k4 | head -1) >> /tmp/high_memory_pod
-echo cluster4 $(kubectl --kubeconfig /root/.kube/clusters/cluster4_config top pods -A --no-headers | sort -nr -k4 | head -1) >> /tmp/high_memory_pod
+echo cluster1 $(kubectl --context cluster1 top pods -A --no-headers | sort -nr -k4 | head -1) > /tmp/high_memory_pod
+echo cluster2 $(kubectl --context cluster2 top pods -A --no-headers | sort -nr -k4 | head -1) >> /tmp/high_memory_pod
+echo cluster3 $(kubectl --context cluster3 top pods -A --no-headers | sort -nr -k4 | head -1) >> /tmp/high_memory_pod
+echo cluster4 $(kubectl --context cluster4 top pods -A --no-headers | sort -nr -k4 | head -1) >> /tmp/high_memory_pod
 final_value=$(cat /tmp/high_memory_pod | sort -nr -k 5 | awk '{print $1,$2,$3}' | head -1 | tr " " ,)
 if [[ $(cat /opt/high_memory_pod | grep $final_value) ]]
 then 
