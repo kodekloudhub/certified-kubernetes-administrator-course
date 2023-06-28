d654ff8470427020aec5b191d17e218adf8a7729
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2023-06-28 05:54:18 +0100

    Update GPG

--- apple-silicon/scripts/04-kube-components.sh
+++ apple-silicon/scripts/04-kube-components.sh
@@ -6,7 +6,7 @@ KUBE_VERSION=1.26.4
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
