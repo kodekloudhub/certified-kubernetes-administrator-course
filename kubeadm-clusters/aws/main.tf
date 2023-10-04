c76188a3b1866c3f4330866eeb0374102208bf81
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2023-10-04 07:45:16 +0100

    Update outputs

--- kubeadm-clusters/aws/main.tf
+++ kubeadm-clusters/aws/main.tf
@@ -19,30 +19,37 @@ provider "aws" {
 }
 
 
-output "controlplane_ip" {
+output "address_controlplane" {
+  description = "Public IP of controlplane"
   value = aws_instance.kubenode["controlplane"].public_ip
 }
 
-output "node01_ip" {
+output "address_node01" {
+  description = "Public IP of node01"
   value = aws_instance.kubenode["node01"].public_ip
 }
 
-output "node02_ip" {
+output "address_node02" {
+  description = "Public IP of node02"
   value = aws_instance.kubenode["node02"].public_ip
 }
 
 output "connect_controlplane" {
+  description = "SSH command for controlplane"
   value = "ssh ubuntu@controlplane"
 }
 
 output "connect_node01" {
+  description = "SSH command for node01"
   value = "ssh ubuntu@$node01"
 }
 
 output "connect_node02" {
+  description = "SSH command for node02"
   value = "ssh ubuntu@node02"
 }
 
 output "etc-hosts" {
+  description = "Additional lines for /etc/hosts"
   value = "\n${aws_instance.kubenode["controlplane"].public_ip} controlplane\n${aws_instance.kubenode["node01"].public_ip} node01\n${aws_instance.kubenode["node02"].public_ip} node02\n"
 }
