fdf75aa5a1e5b6e2cbd0bce45050cb101b1e0136
srinivas-kk <127077815+srinivas-kk@users.noreply.github.com>
2023-10-10 21:47:16 +0530

    added the scripts for validations

---
+++ metrics-staging-scripts/high_memory_node.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo cluster1 $(kubectl --context cluster1 top node --no-headers | sort -nr -k4 | head -1) > /tmp/high_memory_node
+echo cluster2 $(kubectl --context cluster2 top node --no-headers | sort -nr -k4 | head -1) >> /tmp/high_memory_node
+echo cluster3 $(kubectl --context cluster3 top node --no-headers | sort -nr -k4 | head -1) >> /tmp/high_memory_node
+echo cluster4 $(kubectl --context cluster4 top node --no-headers | sort -nr -k4 | head -1) >> /tmp/high_memory_node
+echo cluster5 $(kubectl --context cluster5 top node --no-headers | sort -nr -k4 | head -1) >> /tmp/high_memory_node
+
+final_value=$(cat /tmp/high_memory_node | sort -nr -k 5 | awk '{print $1,$2}' | head -1 | tr " " ,)
+if [[ $(cat /opt/high_memory_node | grep $final_value) ]]
+then 
+        echo SUCCESS
+else
+        echo FAIL
+fi
---
+++ metrics-staging-scripts/high_memory_pod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo cluster1 $(kubectl --context cluster1 top pods -A --no-headers | sort -nr -k4 | head -1) > /tmp/high_memory_pod
+echo cluster2 $(kubectl --context cluster2 top pods -A --no-headers | sort -nr -k4 | head -1) >> /tmp/high_memory_pod
+echo cluster3 $(kubectl --context cluster3 top pods -A --no-headers | sort -nr -k4 | head -1) >> /tmp/high_memory_pod
+echo cluster4 $(kubectl --context cluster4 top pods -A --no-headers | sort -nr -k4 | head -1) >> /tmp/high_memory_pod
+echo cluster5 $(kubectl --context cluster5 top pods -A --no-headers | sort -nr -k4 | head -1) >> /tmp/high_memory_pod
+final_value=$(cat /tmp/high_memory_pod | sort -nr -k 5 | awk '{print $1,$2,$3}' | head -1 | tr " " ,)
+if [[ $(cat /opt/high_memory_pod | grep $final_value) ]]
+then 
+        echo SUCCESS
+else
+        echo FAIL
+fi
