4a48e5dce6bf3b726a2bad515b44ec095c1180dc
fireflycons <34012094+fireflycons@users.noreply.github.com>
2024-02-22 15:25:34 +0000

    set INTERNAL_IP

--- kubeadm-clusters/apple-silicon/scripts/01-setup-hosts.sh
+++ kubeadm-clusters/apple-silicon/scripts/01-setup-hosts.sh
@@ -1,1 +1,4 @@
 cat /tmp/hostentries | sudo tee -a /etc/hosts
+ADDRESS="$(ip -4 addr show enp0s1 | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
+# Export internal IP as an environment variable
+echo "INTERNAL_IP=${ADDRESS}" | sudo tee -a /etc/environment > /dev/null
