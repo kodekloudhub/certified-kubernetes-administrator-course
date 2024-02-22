3343f1fd108b2ec1c0357cff0b18945782876400
fireflycons <34012094+fireflycons@users.noreply.github.com>
2024-02-22 15:25:18 +0000

    Fix delete script

--- kubeadm-clusters/apple-silicon/delete-virtual-machines.sh
+++ kubeadm-clusters/apple-silicon/delete-virtual-machines.sh
@@ -2,7 +2,10 @@
 
 set -eo pipefail
 
-NUM_WORKER_NODES=1
+NUM_WORKER_NODES=2
+MEM_GB=$(( $(sysctl hw.memsize | cut -d ' ' -f 2) /  1073741824 ))
+
+[ $MEM_GB -lt 16 ] && NUM_WORKER_NODES=1
 
 workers=$(for n in $(seq 1 $NUM_WORKER_NODES) ; do echo -n "node0$n " ; done)
 
