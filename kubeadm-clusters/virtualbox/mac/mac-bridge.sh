748487d92eb09e93e5e5fe298c4eb70d00ccbabf
fireflycons <34012094+fireflycons@users.noreply.github.com>
2024-03-07 06:16:43 +0000

    mac bridge selection

---
+++ kubeadm-clusters/virtualbox/mac/mac-bridge.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+iface=$(netstat -rn -f inet | grep '^default' | sort | head -1 | awk '{print $4}')
+#interface=$(VBoxManage list bridgedifs | awk '/^Name:/ { print }' | sed -e 's/^Name:[ ]*\(.*\)/\1/' | grep $iface)
+interface=$(cat ifs.txt | awk '/^Name:/ { print }' | sed -e 's/^Name:[ ]*\(.*\)/\1/' | grep $iface)
+echo -n $interface
